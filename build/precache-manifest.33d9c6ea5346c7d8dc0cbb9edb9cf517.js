9821de092abf0f7eac52b3d25dc2996a894ba46e
Kapitonov Roman <kapitroman@yandex.ru>
2025-03-09 18:29:19 +0300

    добавляет в репозиторий

---
+++ build/precache-manifest.33d9c6ea5346c7d8dc0cbb9edb9cf517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5050bc566872849497afb08b0732a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0947d90cd3b7c8dbff",
+    "url": "/static/css/main.b869fbbd.chunk.css"
+  },
+  {
+    "revision": "84fecf5baaf8e8ea66f4",
+    "url": "/static/js/2.0aab595a.chunk.js"
+  },
+  {
+    "revision": "8d98233dfb2f1a8b874a10cb1448b7bd",
+    "url": "/static/js/2.0aab595a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0947d90cd3b7c8dbff",
+    "url": "/static/js/main.70579bbb.chunk.js"
+  },
+  {
+    "revision": "984c033ff6868392193c",
+    "url": "/static/js/runtime-main.ac09b633.js"
+  },
+  {
+    "revision": "a552303ddb5867c86c7ac5498c8191e7",
+    "url": "/static/media/times-solid.a552303d.svg"
+  }
+]);
